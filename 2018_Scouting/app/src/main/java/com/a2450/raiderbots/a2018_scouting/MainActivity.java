6959660bc83c09732a1407d70502738648cb6412
CDHRaiderBots <raiderbots@c-dh.org>
2018-01-20 11:58:24 -0600

    adding scouting

---
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/MainActivity.java
@@ -0,0 +1,13 @@
+package com.a2450.raiderbots.a2018_scouting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
