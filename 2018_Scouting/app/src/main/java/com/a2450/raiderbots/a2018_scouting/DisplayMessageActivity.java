517e55cd5e579ee44d6a6f42cb113404a5656386
Jaden Pleasants <jdnp2000@gmail.com>
2018-01-27 11:27:42 -0600

    Added display activity, corrected a typo

---
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DisplayMessageActivity.java
@@ -0,0 +1,21 @@
+package com.a2450.raiderbots.a2018_scouting;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        //get intent that launched activity, and extract the relevant string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+}
