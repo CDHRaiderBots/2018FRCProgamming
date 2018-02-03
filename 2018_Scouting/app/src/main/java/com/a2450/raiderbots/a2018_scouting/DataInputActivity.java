1e7ded19090f7c8038734a01e9874a0c1ed2e427
Jaden Pleasants <10004812+Burnergames@users.noreply.github.com>
2018-02-03 14:47:56 -0600

    Fixed layouts, added timer code, added parts of an export function. currently crashes the app.

--- 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
@@ -1,12 +1,33 @@
 package com.a2450.raiderbots.a2018_scouting;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class DataInputActivity extends AppCompatActivity {
+    Handler handler;
+    long startTime, MillisecondTime, TimeBuff, UpdateTime = 0L ;
+    TextView timerText;
+    boolean isTimerRunning;
+    Context myContext;
+    String teamNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +38,76 @@ public class DataInputActivity extends AppCompatActivity {
         //get intent that launched activity, and extract the relevant string
         Intent intent = getIntent();
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        teamNumber = message;
         getSupportActionBar().setTitle("Team " + message);
         NumberPicker ClimbingFauxBarNumberPicker = findViewById(R.id.ClimbingFauxBarNumberPicker);
         ClimbingFauxBarNumberPicker.setMinValue(0);
         ClimbingFauxBarNumberPicker.setMaxValue(2);
         ClimbingFauxBarNumberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        timerText = findViewById(R.id.TimerTime);
+        handler = new Handler();
+        isTimerRunning = false;
+        myContext = this;
     }
+    public void toggleStopwatch(View view) {
+        if (isTimerRunning == false) {
+            isTimerRunning = true;
+            startTime = SystemClock.uptimeMillis();
+            handler.postDelayed(runnable, 0);
+        }
+        else {
+            handler.removeCallbacks(runnable);
+            isTimerRunning = false;
+        }
+    }
+    public Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            MillisecondTime = SystemClock.uptimeMillis() - startTime;
+            long seconds = MillisecondTime / 1000;
+            timerText.setText("" + seconds);
+            handler.postDelayed(this, 0);
+        }
+    };
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    public void export(View view){
+        String sFileName = "TestName";
+        String sBody = "O hai mark";
+        try {
+            File root = new File(Environment.getExternalStorageDirectory(), "Notes");
+            if (!root.exists()) {
+                root.mkdirs();
+            }
+            File gpxfile = new File(root, sFileName);
+            FileWriter writer = new FileWriter(gpxfile);
+            writer.append(sBody);
+            writer.flush();
+            writer.close();
+            Toast.makeText(myContext, "Saved", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        finish();
+    }
+    void createExternalStoragePublicText() {
+        File path = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(path, "team" + teamNumber + ".txt");
+    }
+    /*
+    TODO finish this based on https://developer.android.com/reference/android/os/Environment.html#getExternalStoragePublicDirectory(java.lang.String) and https://developer.android.com/training/data-storage/files.html
+     */
 }
--- 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/MainActivity.java
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/MainActivity.java
@@ -1,28 +1,39 @@
 package com.a2450.raiderbots.a2018_scouting;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.a2450.raiderbots.a2018_scouting.MESSAGE";
+    Context myContext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
+        myContext = this;
 
     }
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DataInputActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+        if (message.isEmpty()) {
+            Toast.makeText(myContext, "no", Toast.LENGTH_SHORT).show();
+        } else {
+            //Toast.makeText(myContext, "Message text is: " + message, Toast.LENGTH_SHORT).show();
+            intent.putExtra(EXTRA_MESSAGE, message);
+            startActivity(intent);
+        }
+
     }
 }
