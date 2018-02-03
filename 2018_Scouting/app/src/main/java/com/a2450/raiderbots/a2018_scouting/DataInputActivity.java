84121171472150d9a9b27144bfdd8e214b49a7f8
Jaden Pleasants <10004812+Burnergames@users.noreply.github.com>
2018-02-03 11:48:26 -0600

    Switched to constraint layouts

--- 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.NumberPicker;
 
 public class DataInputActivity extends AppCompatActivity {
@@ -22,5 +23,8 @@ public class DataInputActivity extends AppCompatActivity {
         ClimbingFauxBarNumberPicker.setMinValue(0);
         ClimbingFauxBarNumberPicker.setMaxValue(2);
         ClimbingFauxBarNumberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+    }
+    public void toggleStopwatch(View view) {
+        
     }
 }
