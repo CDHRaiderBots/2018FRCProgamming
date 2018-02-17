6b55a080a6c7a554bda79f4ffd6acd0b2d395c96
Jaden Pleasants <10004812+Burnergames@users.noreply.github.com>
2018-02-16 20:14:54 -0600

    Added really bad export function that someone should fix and a scrollview for the data entry screen. Also, the app kinda works.

--- 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
+++ 2018_Scouting/app/src/main/java/com/a2450/raiderbots/a2018_scouting/DataInputActivity.java
@@ -1,5 +1,6 @@
 package com.a2450.raiderbots.a2018_scouting;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Environment;
@@ -12,7 +13,11 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.NumberPicker;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import java.io.File;
 import java.io.FileOutputStream;
@@ -29,13 +34,26 @@ public class DataInputActivity extends AppCompatActivity {
     Context myContext;
     String teamNumber;
     private Toolbar toolbar;
+    Boolean autoBaseLine;
+    Boolean autoCubeDeposit;
+    int blocksScale;
+    Button scaleButton;
+    int blocksBlue;
+    int blocksRed;
+    Button teleopBlueButton;
+    Button teleopRedButton;
+    Boolean canGetCubesFromPortal;
+    Boolean canGetCubesFromPile;
+    Boolean climbingSuccess;
+    String malfunctionType;
+    long climbingTime;
+    int robotsHeld;
+    EditText miscNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data_input);
-        //Toolbar myToolbar = findViewById(R.id.my_toolbar);
-        //setSupportActionBar(myToolbar);
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
         //get intent that launched activity, and extract the relevant string
@@ -43,14 +61,29 @@ public class DataInputActivity extends AppCompatActivity {
         String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
         teamNumber = message;
         getSupportActionBar().setTitle("Team " + message);
+        /* This is temporarily being removed.
         NumberPicker ClimbingFauxBarNumberPicker = findViewById(R.id.ClimbingFauxBarNumberPicker);
         ClimbingFauxBarNumberPicker.setMinValue(0);
         ClimbingFauxBarNumberPicker.setMaxValue(2);
         ClimbingFauxBarNumberPicker.setDescendantFocusability(NumberPicker.FOCUS_BLOCK_DESCENDANTS);
+        */
         timerText = findViewById(R.id.TimerTime);
         handler = new Handler();
         isTimerRunning = false;
         myContext = this;
+        autoBaseLine = null;
+        autoCubeDeposit = null;
+        blocksScale = 0;
+        scaleButton = findViewById(R.id.ScaleButton);
+        blocksBlue = 0;
+        blocksRed = 0;
+        teleopBlueButton = findViewById(R.id.BlueSwitchButton);
+        teleopRedButton = findViewById(R.id.RedSwitchButton);
+        canGetCubesFromPile = null;
+        canGetCubesFromPortal = null;
+        malfunctionType = null;
+        robotsHeld = 0;
+        miscNotes = findViewById(R.id.MiscNotes);
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -69,14 +102,14 @@ public class DataInputActivity extends AppCompatActivity {
         if (id == R.id.Export) {
             Date currentTime = Calendar.getInstance().getTime();
             String filename = teamNumber + " - " + currentTime;
-            writeToFile(filename, "text");
+            writeToFile(filename);
             finish();
         }
 
         return super.onOptionsItemSelected(item);
     }
     public void toggleStopwatch(View view) {
-        if (isTimerRunning == false) {
+        if (!isTimerRunning) {
             isTimerRunning = true;
             startTime = SystemClock.uptimeMillis();
             handler.postDelayed(runnable, 0);
@@ -88,12 +121,76 @@ public class DataInputActivity extends AppCompatActivity {
             Log.v("stopwatch","Stopwatch stopped");
         }
     }
+    public void onCheckBoxClick(View view) {
+        Boolean checked = ((CheckBox) view).isChecked();
+        switch(view.getId()) {
+            case R.id.AutoBaseLine:
+                autoBaseLine = checked;
+                break;
+            case R.id.AutoCubeDeposit:
+                autoCubeDeposit = checked;
+                break;
+            case R.id.PortalCheckbox:
+                canGetCubesFromPortal = checked;
+                break;
+            case R.id.PileCheckbox:
+                canGetCubesFromPile = checked;
+                break;
+
+        }
+    }
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        switch (view.getId()) {
+            case R.id.SuccessfulClimb:
+                climbingSuccess = true;
+                break;
+            case R.id.FailedClimb:
+                climbingSuccess = false;
+                break;
+            case R.id.DidntClimb:
+                climbingSuccess = null;
+                break;
+            case R.id.CompleteMalfunction:
+                malfunctionType = "Complete malfunction";
+                break;
+            case R.id.PartialMalfunction:
+                malfunctionType = "Partial malfunction";
+                break;
+            case R.id.NoMalfunction:
+                malfunctionType = "No malfunction";
+                break;
+            case R.id.HoldsZeroRobots:
+                robotsHeld = 0;
+                break;
+            case R.id.HoldsOneRobot:
+                robotsHeld = 1;
+                break;
+            case R.id.HoldsTwoRobots:
+                robotsHeld = 2;
+                break;
+        }
+    }
+    public void incrementBlocksScale(View view) {
+        blocksScale ++;
+        scaleButton.setText("Scale: " + blocksScale);
+    }
+    public void incrementBlocksBlue(View view) {
+        blocksBlue ++;
+        teleopBlueButton.setText("Blue Switch: " + blocksBlue);
+    }
+    public void incrementBlocksRed(View view) {
+        blocksRed ++;
+        teleopRedButton.setText("Red Switch: " + blocksRed);
+    }
+
     public Runnable runnable = new Runnable() {
         @Override
         public void run() {
             MillisecondTime = SystemClock.uptimeMillis() - startTime;
             long seconds = MillisecondTime / 1000;
             timerText.setText("" + seconds);
+            climbingTime = seconds;
             handler.postDelayed(this, 0);
         }
     };
@@ -115,12 +212,17 @@ public class DataInputActivity extends AppCompatActivity {
     public void export(View view){
         Date currentTime = Calendar.getInstance().getTime();
         String filename = teamNumber + " - " + currentTime;
-        writeToFile(filename, "text");
+        writeToFile(filename);
         finish();
     }
 
-    public void writeToFile(String filename, String data)
+    public void writeToFile(String filename)
     {
+        // generate data
+        String data = teamNumber + "," + autoBaseLine + "," + autoCubeDeposit + "," + blocksScale + "," + blocksRed + "," + blocksBlue + "," + canGetCubesFromPortal + "," + canGetCubesFromPile + "," + robotsHeld + "," + climbingTime + "," + climbingSuccess + "," + malfunctionType + ",\"" + miscNotes.getText().toString() + "\"";
+        //TODO: make this significantly less bad.
+        //FIXME: I already know this won't work assuming the text contains a comma between two double quotes, but that can be fixed later.
+
         // Get the directory for the user's public pictures directory.
         final File path =
                 Environment.getExternalStoragePublicDirectory
